1f9fb5bfb71a6754d831db27d3919c0433159ab9
tan.pham <tan.pham@tx-tech.com>
2022-07-29 15:23:44 +0700

    Init

---
+++ config/middlewares.js
@@ -0,0 +1,23 @@
+module.exports = [
+  'strapi::errors',
+  // 'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'img-src': ["'self'", 'data:', 'cdn.jsdelivr.net', '*'],
+        },
+      }
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
